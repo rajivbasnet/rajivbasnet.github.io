939c33b2e8f00aecb060e4d015b4693f1089ffda
rajivbasnet <net.rajivbas@gmail.com>
2020-09-18 00:07:42 -0400

    Updates

---
+++ precache-manifest.e997f4ee36f920abf4025a39a8752602.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44783e8c279ba494d254c856cb37e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770d73955910d29b773f",
+    "url": "/static/css/2.1432724e.chunk.css"
+  },
+  {
+    "revision": "59c0dd87de098f402337",
+    "url": "/static/css/main.7410880b.chunk.css"
+  },
+  {
+    "revision": "770d73955910d29b773f",
+    "url": "/static/js/2.6af829f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6af829f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c0dd87de098f402337",
+    "url": "/static/js/main.8c3bc7d4.chunk.js"
+  },
+  {
+    "revision": "d637c7d320c7ab18c3a6",
+    "url": "/static/js/runtime-main.bde3a731.js"
+  },
+  {
+    "revision": "8bfbc526553ce0851c021fe99f23ffe9",
+    "url": "/static/media/Arizonia-Regular.8bfbc526.ttf"
+  },
+  {
+    "revision": "b39ffa3879dd8986c9e481d0e4de23da",
+    "url": "/static/media/bgimage1.b39ffa38.jpg"
+  },
+  {
+    "revision": "82ca3dc0e39543a125dc1d105b2907cd",
+    "url": "/static/media/bgimage3.82ca3dc0.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "da21feda28e287b08ecc58268c59014b",
+    "url": "/static/media/handtracking.da21feda.png"
+  },
+  {
+    "revision": "c006a644c154c9df0a6543cceb026c1d",
+    "url": "/static/media/librarysearch.c006a644.png"
+  },
+  {
+    "revision": "1482599cfad73ed1322f92d079de6f72",
+    "url": "/static/media/other.1482599c.png"
+  },
+  {
+    "revision": "8ad153ff347c3b9ef3509984fa9be321",
+    "url": "/static/media/others.8ad153ff.png"
+  },
+  {
+    "revision": "8252fb4f12d1b37b21b674d609748a59",
+    "url": "/static/media/predicting.8252fb4f.png"
+  },
+  {
+    "revision": "41ad06aa1ba1535f6da0e34e9208b1f9",
+    "url": "/static/media/profilepic.41ad06aa.jpg"
+  },
+  {
+    "revision": "3b8b8c02ca9cf463abe44cca24115fac",
+    "url": "/static/media/quantmod.3b8b8c02.png"
+  },
+  {
+    "revision": "16c2dc441642874eaeafde79827f94ce",
+    "url": "/static/media/videorenting.16c2dc44.png"
+  }
+]);
